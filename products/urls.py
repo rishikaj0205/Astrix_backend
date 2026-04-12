625b0fdd9975dd90df69e53645a690156ab3c01d
rishikaj0205 <rishikaj0205@gmail.com>
2026-04-12 17:47:00 +0530

    add

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import get_products, upload_product
 
 urlpatterns = [
-    path("productsadd/", get_products),
+    path("products/", get_products),
     path("upload-product/", upload_product),
 ]
